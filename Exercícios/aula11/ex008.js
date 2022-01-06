6cd7117b0216ef5647526327281ac04141713219
vattghern <otavioaugustobrb19@gmail.com>
2022-01-05 21:49:37 -0300

    if and else pt.1

---
+++ Exercícios/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é de ${vel}Km por hora`)
+
+if (vel > 60) {
+    console.log('Você passou da velocidade limite(60km/h). Toma multa, otário.')
+} else {
+    console.log('Você está dentro do limite de velocidade. Continue dirigindo com cuidado.')
+}
---
+++ Exercícios/aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'EUA'
+
+console.log(`Você vive no(s) ${país}.`)
+
+if (país == 'Brasil') {
+    console.log('Hue hue BR!')
+} else {
+    console.log('És gringo!')
+}
