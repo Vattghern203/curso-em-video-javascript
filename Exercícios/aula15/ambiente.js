34a282a70cdb5b6076772f351d45195300157a77
vattghern <otavioaugustobrb19@gmail.com>
2022-03-16 14:29:15 -0300

    Arrays

---
+++ Exercícios/aula15/ambiente.js
@@ -0,0 +1,15 @@
+let num = [1, 6, 7, 10, 9]
+
+num.push(8)
+
+console.log(`Nosso vetor é o ${num}`)
+
+num.sort()
+
+console.log(`Nosso vetor é o ${num}`)
+
+console.log(`O vetor tem ${num.length} posições`)
+
+
+
+
---
+++ Exercícios/aula15/looping_vetor.js
@@ -0,0 +1,22 @@
+var v = [4, 7, 1, 0, 3, 2, 9, 8, 6, 5]
+
+v.sort()
+
+for(var i = 0; i < 10; i++) {
+    console.log(`Vetor de número ${i} e seu valor é de: ${v[i]}`)
+}
+
+for(var k in v) {
+    console.log(v[k])
+}
+
+let pos = v.indexOf(1)
+
+if (pos == -1) {
+    console.log('Valor não encontrado.')
+}
+
+else {
+    console.log(`O vetor buscado se encontra na posição: ${pos}`)
+}
+
