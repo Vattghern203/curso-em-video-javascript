7e5c7b9517caa4c97c529a2f6e807ccc197a4ae2
vattghern <otavioaugustobrb19@gmail.com>
2022-04-12 15:41:12 -0300

    Aula 16

--- Exercícios/aula15/ambiente.js
+++ Exercícios/aula15/ambiente.js
@@ -4,11 +4,22 @@ num.push(8)
 
 console.log(`Nosso vetor é o ${num}`)
 
-num.sort()
+//num.sort()
 
 console.log(`Nosso vetor é o ${num}`)
 
 console.log(`O vetor tem ${num.length} posições`)
+
+let pos = num.indexOf(10)
+
+if (pos == -1) {
+    console.log(`Valor não encontrado`)
+}
+
+else {
+    console.log(`O valor 10 está na posição ${pos}`)
+}
+
 
 
 
--- Exercícios/aula15/looping_vetor.js
+++ Exercícios/aula15/looping_vetor.js
@@ -1,6 +1,9 @@
 var v = [4, 7, 1, 0, 3, 2, 9, 8, 6, 5]
 
+
 v.sort()
+
+/*
 
 for(var i = 0; i < 10; i++) {
     console.log(`Vetor de número ${i} e seu valor é de: ${v[i]}`)
@@ -20,3 +23,8 @@ else {
     console.log(`O vetor buscado se encontra na posição: ${pos}`)
 }
 
+*/
+
+for(let pos in v) {
+    console.log(`A posição ${pos} tem o valor ${v[pos]}`)
+}
